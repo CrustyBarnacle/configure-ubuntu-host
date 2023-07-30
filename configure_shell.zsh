fe0e7109a6dbed12f0dea3ad283a69363cf1a5f3
CrustyBarnacle <915931+CrustyBarnacle@users.noreply.github.com>
2023-07-30 16:16:14 -0700

    Adds useful package management aliases.

--- configure_shell.zsh
+++ configure_shell.zsh
@@ -15,3 +15,21 @@ exec zsh # Powerline10k wizard should start automatically
 # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${$HOME}/.zshrc
 echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${$HOME}/.zshrcS
+
+# Package management aliases
+# Create/append to `aliases_apt.zsh`
+cat <<- EOF >> ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/aliases_apt.zsh
+# Package manager aliases (apt, firmware, flatpak)
+
+# APT
+alias au="sudo apt update"
+alias upgrade="sudo apt upgrade -y && sudo apt dist-upgrade && sudo apt autoremove -y"
+alias clean="sudo apt autoclean"
+alias apt-all="au && upgrade && clean"
+
+# Firmware
+alias fwu="sudo fwupdmgr get-devices && sudo fwupdmgr get-updates && sudo fwupdmgr update -y"
+
+# Flatpak
+alias fpu="flatpak update --assumeyes && flatpak upgrade --assumeyes"
+EOF
